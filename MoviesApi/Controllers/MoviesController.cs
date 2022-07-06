bd9ff9724fe253f2258cd238ad14fe8138b8f62b
Ramadan <r.eita@wesamsoft.com>
2022-07-06 16:49:58 +0200

    Adding MoviesController End Points

--- MoviesApi/Controllers/MoviesController.cs
+++ MoviesApi/Controllers/MoviesController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace MoviesApi.Controllers
 {
@@ -8,7 +7,7 @@ namespace MoviesApi.Controllers
     public class MoviesController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-        private new List<string> _allowedExtenstions=new List<string> { ".jpg",".png"};
+        private new List<string> _allowedExtenstions = new List<string> { ".jpg", ".png" };
         private long _maxAllowedPosterSize = 1048576;
 
         public MoviesController(ApplicationDbContext context)
@@ -16,23 +15,93 @@ namespace MoviesApi.Controllers
             _context = context;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> CreateAsync( [FromForm]MovieDto dto)
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
         {
-            if (!_allowedExtenstions.Contains(Path.GetExtension(dto.Poster.FileName).ToLower()))
-         
-                return BadRequest("only .png and Jpg images are allowed");
+            var movies = await _context.Movies.OrderByDescending(r=>r.Rate).Include(m => m.Genre)
+                .Select(m => new MovieDetailsDto
+                {
+                    Id = m.Id,
+                    GenreId = m.GenreId,
+                    GenreName = m.Genre.Name,
+                    Poster = m.Poster,
+                    Rate = m.Rate,
+                    Storeline = m.Storeline,
+                    Title = m.Title,
+                    Year = m.Year
+
+
+                }).ToListAsync();
+            return Ok(movies);
+        }
+
+        [HttpGet ("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var movie = await _context.Movies.Include(m => m.Genre).SingleOrDefaultAsync(m => m.Id == id);
+            if (movie == null)
+              
+                return NotFound();
+
+            var dto = new MovieDetailsDto
+            {
+                Id = movie.Id,
+                GenreId = movie.GenreId,
+                GenreName = movie.Genre?.Name,
+                Poster = movie.Poster,
+                Rate = movie.Rate,
+                Storeline = movie.Storeline,
+                Title = movie.Title,
+                Year = movie.Year
+            };
+            return Ok(dto);
             
+        }
+
+        [HttpGet("{GetByGenraIdAsync}")]
+        public async Task<IActionResult> GetByGenraIdAsync(byte genraId)
+        {
+
+            var movies = await _context.Movies
+                .Where(m=>m.GenreId==genraId)
+                .OrderByDescending(r => r.Rate).Include(m => m.Genre)
+                .Select(m => new MovieDetailsDto
+                {
+                    Id = m.Id,
+                    GenreId = m.GenreId,
+                    GenreName = m.Genre.Name,
+                    Poster = m.Poster,
+                    Rate = m.Rate,
+                    Storeline = m.Storeline,
+                    Title = m.Title,
+                    Year = m.Year
+
+
+                }).ToListAsync();
+            return Ok(movies); 
+
+        }
+
+            [HttpPost]
+        public async Task<IActionResult> CreateAsync([FromForm] MovieDto dto)
+        {
+            if (dto.Poster == null)
+                return BadRequest("poster is requerd !");
+
+            if (!_allowedExtenstions.Contains(Path.GetExtension(dto.Poster.FileName).ToLower()))
+
+                return BadRequest("only .png and Jpg images are allowed");
+
 
             if (dto.Poster.Length > _maxAllowedPosterSize)
-            
+
                 return BadRequest("Max allow size for poster is 1M");
-          
+
             var isValidGenra = await _context.Genres.AnyAsync(g => g.Id == dto.GenreId);
             if (!isValidGenra)
-            
+
                 return BadRequest("Invalid genera ID !");
-          
+
             using var dataStreem = new MemoryStream();
             await dto.Poster.CopyToAsync(dataStreem);
             var movie = new Movie
@@ -48,9 +117,66 @@ namespace MoviesApi.Controllers
             };
             await _context.AddAsync(movie);
             _context.SaveChanges();
-            return Ok(movie); 
-               
+            return Ok(movie);
 
+
+        }
+        [HttpPut("id")]
+
+        public async Task<IActionResult> UpdateAsync(int id , [FromForm] MovieDto dto)
+
+        {
+
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+                return NotFound($"No Movie was Found with id {id} ");
+
+            var isValidGenra = await _context.Genres.AnyAsync(g => g.Id == dto.GenreId);
+            if (!isValidGenra)
+
+                return BadRequest("Invalid genera ID !");
+
+            if (dto.Poster != null)
+            {
+                if (!_allowedExtenstions.Contains(Path.GetExtension(dto.Poster.FileName).ToLower()))
+
+                    return BadRequest("only .png and Jpg images are allowed");
+
+
+                if (dto.Poster.Length > _maxAllowedPosterSize)
+
+                    return BadRequest("Max allow size for poster is 1M");
+
+
+                using var dataStreem = new MemoryStream();
+                await dto.Poster.CopyToAsync(dataStreem);
+                movie.Poster = dataStreem.ToArray();
+            }
+
+
+            movie.Title = dto.Title;
+            movie.Year = dto.Year;
+            movie.GenreId = dto.GenreId;
+         
+            movie.Storeline = dto.Storeline;
+           
+            movie.Rate = dto.Rate;  
+            _context.SaveChanges();
+            return Ok(movie);
+
+        }
+
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+                return NotFound($"No Movie was Found with id {id} ");
+
+            _context.Remove(movie);
+            _context.SaveChanges();
+            return Ok(movie);
         }
     }
 }
--- MoviesApi/Dtos/MovieDetailsDto.cs
+++ MoviesApi/Dtos/MovieDetailsDto.cs
@@ -1,10 +1,8 @@
 ﻿namespace MoviesApi.Dtos
 {
-    public class MovieDto
+    public class MovieDetailsDto
     {
-      
-
-        [MaxLength(250)]
+        public int Id { get; set; }
 
         public string Title { get; set; }
 
@@ -12,13 +10,12 @@
 
         public double Rate { get; set; }
 
-        [MaxLength(2500)]
         public string Storeline { get; set; }
 
-        public IFormFile? Poster { get; set; }
+        public byte[] Poster { get; set; }
 
         public byte GenreId { get; set; }
 
-        
+        public string GenreName { get; set; }
     }
 }
